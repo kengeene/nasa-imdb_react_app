bfe0523ddf654212c2bd5dc37262b0f92007def9
kengeene <eugene@sendyit.com>
2022-10-03 21:08:10 +0300

    Add react router

--- src/components/MovieDisplay/index.js
+++ src/components/MovieDisplay/index.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-console */
 /* eslint-disable camelcase */
 import { useEffect, useState } from 'react';
+import { Outlet, Link } from 'react-router-dom';
 import { fetchMovieInfo } from '../../requests';
 
 /* eslint-disable react/prop-types */
@@ -352,7 +353,7 @@ function MovieDisplay() {
     const getMovieInfo = async () => {
       //   const { data } = await fetchMovieInfo();
       //   setMovies(data.results);
-      console.log('movies', movies);
+      // console.log('movies', movies);
     };
     getMovieInfo();
   }, []);
@@ -361,7 +362,7 @@ function MovieDisplay() {
     <div className="grid-container">
       {movies.map(({ original_title, overview, release_date, id, popularity, poster_path }) => (
         <div className="grid-item movie" key={id}>
-          <a href={`${baseImgUrl}${poster_path}`} className="movie-link">
+          <Link to={`movies/${id}`} className="movie-link">
             <img
               src={
                 poster_path
@@ -380,7 +381,7 @@ function MovieDisplay() {
             <p className="movie-release-date">
               Release Date: <em>{release_date}</em>
             </p>
-          </a>
+          </Link>
         </div>
       ))}
     </div>
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './assets/index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import ErrorPage from './components/ErrorPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <ErrorPage />
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
